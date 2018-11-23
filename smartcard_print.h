0ec20e97fd9022cd6cf74d6a1e4c544405b46a81
Philippe Thierry <phil@reseau-libre.net>
2018-11-23 11:10:05 +0100

    [enhancement] add unified SMARTCARD_DEBUG debugging toggle mechanism

--- smartcard_print.h
+++ smartcard_print.h
@@ -1,13 +1,14 @@
 #ifndef __SMARTCARD_PRINT_H__
 #define __SMARTCARD_PRINT_H__
 
+#include "autoconf.h"
 #include "api/print.h"
 
-#define SMARTCARD_DEBUG
+#define SMARTCARD_DEBUG CONFIG_SMARTCARD_DEBUG
 #define MEASURE_TOKEN_PERF
 
 /* Primitive for debug output */
-#ifdef SMARTCARD_DEBUG
+#if SMARTCARD_DEBUG
 #define log_printf(...) printf(__VA_ARGS__)
 #else
 #define log_printf(...)
